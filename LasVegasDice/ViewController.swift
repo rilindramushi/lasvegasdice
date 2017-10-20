c390ad551f8732dc5bb620e39f0db18db09c2c8a
Rilind Ramushi <rilind.ramushi@gmail.com>
2017-10-20 02:28:15 +0200

    Initial Commit

--- LasVegasDice/ViewController.swift
+++ LasVegasDice/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var Fotoja1: UIImageView!
+    @IBOutlet weak var Fotoja2: UIImageView!
+    
+    var fotot = ["zari1", "zari2", "zari3", "zari4", "zari5", "zari6"]
+    
+    var fotot1 = ["zari1", "zari2", "zari3", "zari4", "zari5", "zari6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    
+    @IBAction func rollButton(_ sender: Any) {
+         luajLojen()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        if motion == UIEventSubtype.motionShake{
+            
+            luajLojen()
+        }
+    }
+    
+    
+    
+    func luajLojen(){
+        var zariRandom:Int = Int(arc4random_uniform(UInt32(fotot.count)))
+        var zariRandom2:Int = Int(arc4random_uniform(UInt32(fotot1.count)))
+        
+        var zariQeDel:String = (fotot[zariRandom])
+        var zariQeDel2:String = (fotot1[zariRandom2])
+        
+        Fotoja1.image = UIImage.init(named:zariQeDel)
+        Fotoja2.image = UIImage.init(named:zariQeDel2)
 
-
+    }
 }
-
